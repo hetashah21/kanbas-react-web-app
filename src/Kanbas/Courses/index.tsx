7862a709e665d057ac2de1220647084cfb7f4d7e
Heta Shah <hetashah@Hetas-MacBook-Air.local>
2024-10-17 20:13:29 -0700

    Implemented Assignment 3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CoursesNavigation from "./Navigation";
-import { Navigate, Route, Routes } from "react-router";
+import { courses } from "../Database";
+import { Navigate, Route, Routes,useParams, useLocation } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
@@ -8,9 +9,12 @@ import AssignmentEditor from "./Assignments/Editor";
 import PeopleTable from "./People/Table";
 import { FaAlignJustify } from "react-icons/fa";
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
     return (
       <div id="wd-courses">
-        <h2 className = "text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" /> Course 1234 </h2>
+        <h2 className = "text-danger"><FaAlignJustify className="me-4 fs-4 mb-1" />    {course && course.name}  &gt; {pathname.split("/")[4]} </h2>
         <hr />
         <div className="d-flex">
           <div className="d-none d-md-block">
---
+++ src/Kanbas/Database/index.tsx
@@ -0,0 +1,6 @@
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import { useParams } from "react-router";
+export default {
+  courses, modules, assignments, };

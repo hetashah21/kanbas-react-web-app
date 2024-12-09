9e19d4a8c07c26ffc59e0e6030183fe37e06c310
Heta Shah <hetashah@Hetas-MacBook-Air.local>
2024-12-08 18:06:45 -0800

    a6 commit 2

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -14,18 +14,60 @@ import Session from "./Account/Session";
 export default function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
     const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const [enrolling, setEnrolling] = useState<boolean>(false);
+    const findCoursesForUser = async () => {
+      try {
+      const courses = await userClient.findCoursesForUser(currentUser._id);
+     setCourses(courses);
+     } catch (error) {
+     console.error(error);
+      }
+    };
+
+    const updateEnrollment = async (courseId: string, enrolled: boolean) => {
+      if (enrolled) {
+        await userClient.enrollIntoCourse(currentUser._id, courseId);
+      } else {
+        await userClient.unenrollFromCourse(currentUser._id, courseId);
+      }
+      setCourses(
+        courses.map((course) => {
+          if (course._id === courseId) {
+            return { ...course, enrolled: enrolled };
+          } else {
+            return course;
+          }
+        })
+      );
+    };
+   
     const fetchCourses = async () => {
       try {
-        const courses = await userClient.findMyCourses();
+        const allCourses = await courseClient.fetchAllCourses();
+        const enrolledCourses = await userClient.findCoursesForUser(
+          currentUser._id
+        );
+        const courses = allCourses.map((course: any) => {
+          if (enrolledCourses.find((c: any) => c._id === course._id)) {
+            return { ...course, enrolled: true };
+          } else {
+            return course;
+          }
+        });
         setCourses(courses);
       } catch (error) {
         console.error(error);
       }
     };
+   
     useEffect(() => {
+      if (enrolling) {
       fetchCourses();
-    }, [currentUser]);
-  
+       } else {
+      findCoursesForUser();
+        }
+    }, [currentUser, enrolling]);
+
     const [course, setCourse] = useState<any>({
       _id: "1234", name: "New Course", number: "New Number",
       startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
@@ -65,7 +107,11 @@ export default function Kanbas() {
                                 setCourse={setCourse}
                                 addNewCourse={addNewCourse}
                                 deleteCourse={deleteCourse}
-                                updateCourse={updateCourse}/> </ProtectedRoute>
+                                updateCourse={updateCourse}
+                                enrolling={enrolling}
+                                setEnrolling={setEnrolling}
+                                updateEnrollment={updateEnrollment}
+                                /> </ProtectedRoute>
                             } />
                             <Route path="/Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses}/></ProtectedRoute>} />
                             <Route path="/Calendar" element={<h1>Calendar</h1>} />
@@ -75,5 +121,9 @@ export default function Kanbas() {
         </div>
         </Session>
     );
+}
+
+function fetchCourses() {
+  throw new Error("Function not implemented.");
 }
   
